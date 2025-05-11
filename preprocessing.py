b984da5090b2ffb6d0d53290d300f7103c66a828
Flavia Macovei <diama.m@macovei.eu>
2025-05-11 11:14:33 +0200

    implement preprocessing
    vocabulary is read from csv file and saved as faiss index

---
+++ preprocessing.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+import faiss
+import sys
+sys.path.append(sys.path[0] + '/..')
+from utils.ConfigManager import ConfigManager as CM
+
+SOURCEFILE = CM().get('data.source')
+DESTFILE = CM().get('data.index')
+
+df = pd.read_csv(SOURCEFILE)["word"].to_frame()
+
+text = df["word"]
+encoder = SentenceTransformer("paraphrase-mpnet-base-v2")
+vectors = encoder.encode(text)
+
+df['vectors'] = vectors.tolist()
+
+vector_dimension = vectors.shape[1]
+index = faiss.IndexFlatL2(vector_dimension)
+faiss.normalize_L2(vectors)
+index.add(vectors)
+
+faiss.write_index(index, DESTFILE)
