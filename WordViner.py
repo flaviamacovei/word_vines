c54354f2e01c8f7c304a12cb3a784b2d24e1e8f5
Flavia Macovei <diama.m@macovei.eu>
2025-08-29 09:52:33 +0200

    set up server side
    127.0.0.1:8000
    
    endpoint /map/{word} returns json with centre word, synonyms, and
    synonym positions
    
    start from command line using
    uvicorn main:app --reload

---
+++ WordViner.py
@@ -0,0 +1,12 @@
+class WordViner():
+    def __init(self):
+        ...
+
+    def in_vocab(self, word: str):
+        return True
+
+    def get_synonyms(self, word: str):
+        return ["dog", "cat", "mouse"]
+
+    def get_positions(self, word: str):
+        return [[0, 1], [2, 3], [4, 5]]
